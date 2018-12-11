e696940cb166ce309a84bc64f203ea1e0b44a4fc
Szymon Soltysiak <szymon.soltysiak@gmail.com>
2018-12-11 22:45:47 +0100

    Minor adjustments. (#1)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const Logo = () => <img src={logo} className="App-logo" alt="logo" />;
+import { Logo } from './shared/components/Logo';
 
 class App extends Component {
   render() {
