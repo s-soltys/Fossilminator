1d7e4bc33ad947864d3d1907529ec72c5d5a0c53
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2018-12-12 00:11:00 +0100

    Basic layout.

--- src/navigation/components/NavigationHeader.js
+++ src/navigation/components/NavigationHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const NavigationHeader = () => (
     <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
-        <span class="navbar-brand">Fixed navbar</span>
+        <span class="navbar-brand">Fossiliminator</span>
         </nav>
     </header>
 );
