d2f51a630c38110fbc35744d6d1033002e93a719
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2018-12-11 23:50:01 +0100

    Use bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
+import './style/style.scss';
+import './style/custom.scss';
 
 import { Logo } from './shared/components/Logo';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Logo />
-          <p>
-            First version of Fossiliminator!
-          </p>
+      <React.Fragment>
+        <header>
+          <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <span class="navbar-brand">Fixed navbar</span>
+          </nav>
         </header>
-      </div>
+        <main role="main" class="container mt-5 pt-3">
+          <div className="row">
+            <div className="col-6">
+              <Logo />
+            </div>
+            <div className="col-6 d-flex align-items-center">
+              First version of Fossiliminator!
+            </div>
+          </div>
+        </main>
+      </React.Fragment>
     );
   }
 }
