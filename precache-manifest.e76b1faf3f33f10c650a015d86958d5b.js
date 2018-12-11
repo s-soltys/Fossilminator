f38f0a8ccc50b252919df5c5f88e7c6ffc9636a8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-11 20:42:24 +0000

    Deploy s-soltys/Fossilminator to github.com/s-soltys/Fossilminator.git:gh-pages

---
+++ precache-manifest.e76b1faf3f33f10c650a015d86958d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5251250cdf316a42c7d5",
+    "url": "/Fossilminator/static/css/main.c141be84.chunk.css"
+  },
+  {
+    "revision": "5251250cdf316a42c7d5",
+    "url": "/Fossilminator/static/js/main.5251250c.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/Fossilminator/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "0ffb6c034d5066701561",
+    "url": "/Fossilminator/static/js/runtime~main.0ffb6c03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Fossilminator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5e21b38727f1ddc122a7b577caf84116",
+    "url": "/Fossilminator/index.html"
+  }
+];
