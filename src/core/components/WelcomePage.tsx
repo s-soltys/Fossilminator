a024e166582fec0cc17e17cb95ecc4497bddcf02
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2018-12-12 07:39:03 +0100

    Fix layout.

--- src/core/components/WelcomePage.tsx
+++ src/core/components/WelcomePage.tsx
@@ -9,10 +9,10 @@ interface Props {
 
 export const WelcomePage = ({ title }: Props) => (
     <div className="row">
-        <div className="col-6">
+        <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
             <Logo />
         </div>
-        <div className="col-6 d-flex align-items-center">
+        <div className="col-12 col-md-6 d-flex align-items-center">
             <Card>
                 <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
                 <CardBody>
