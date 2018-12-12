3ddca54a3334a9c0e6ece4cd81394d2219df77cf
Szymon Soltysiak <szymon.soltysiak@gmail.com>
2018-12-12 22:38:21 +0100

    Run sagas... actually :)

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,15 +1,10 @@
 export * from './actions';
 export * from './constants';
 
-import { Component } from 'react';
-import { createStore, applyMiddleware, compose, Store } from 'redux';
-import createSagaMiddleware from 'redux-saga'
-// import withRedux from 'next-redux-wrapper';
-// import withReduxSaga from 'next-redux-saga';
-// import { RootSaga } from './sagas';
+import { applyMiddleware, createStore, Store } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { AppState, InitialAppState } from './constants';
 import { RootReducer } from './reducers';
-import { InitialAppState } from './constants';
-import { AppState } from './constants';
 import { RootSaga } from './sagas';
 
 // Configure Redux Dev Tools
@@ -18,27 +13,14 @@ import { RootSaga } from './sagas';
 //     composeEnhancers = (window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
 // }
 
-// const sagaMiddleware = createSagaMiddleware();
-
 export const configureStore = (initialState: AppState = InitialAppState): Store<AppState> => {
-    const store: any = createStore(RootReducer);
+    const sagaMiddleware = createSagaMiddleware();
 
-    // store.runSagaTask = () => (store.sagaTask = sagaMiddleware.run(RootSaga));
+    const store: any = createStore(RootReducer, applyMiddleware(sagaMiddleware));
 
-    // store.runSagaTask();
+    store.runSagaTask = () => (store.sagaTask = sagaMiddleware.run(RootSaga as any));
+
+    store.runSagaTask();
 
     return store;
 };
-
-// export const connectWithRedux = (component: Component) => withRedux(configureStore)(withReduxSaga(component as any) as any);
-
-// create the saga middleware
-// const sagaMiddleware = createSagaMiddleware()
-// // mount it on the Store
-// const store = createStore(
-//   RootReducer,
-//   applyMiddleware(sagaMiddleware)
-// )
-
-// // then run the saga
-// sagaMiddleware.run(RootSaga as any);
