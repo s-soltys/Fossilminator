8240f2929e0d478058d27689f8ddff04b9dfd161
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2018-12-12 01:27:57 +0100

    Remaining tsx.

--- src/core/components/WelcomePage.tsx
+++ src/core/components/WelcomePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Logo } from '../../shared/components/Logo';
+import { Translate } from 'react-i18nify';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 interface Props {
@@ -16,9 +17,11 @@ export const WelcomePage = ({ title }: Props) => (
                 <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
                 <CardBody>
                     <CardTitle>{ title || 'Default title' }</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardSubtitle>This is a subtitle...</CardSubtitle>
                     <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    <Button color="info">Start!</Button>
+                    <Button color="info">
+                        <Translate value="actions.calculateUsage" />
+                    </Button>
                 </CardBody>
             </Card>
         </div>
