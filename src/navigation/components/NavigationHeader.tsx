ab68199d4f162e3e98cd5affbb898450ae9bd5d4
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2018-12-12 01:21:49 +0100

    Rename to tsx.

--- src/navigation/components/NavigationHeader.tsx
+++ src/navigation/components/NavigationHeader.tsx
@@ -4,8 +4,8 @@ import { Translate } from 'react-i18nify';
 
 export const NavigationHeader = () => (
     <header>
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
-        <span class="navbar-brand">
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+        <span className="navbar-brand">
             <Translate value="application.title" />
         </span>
         </nav>
