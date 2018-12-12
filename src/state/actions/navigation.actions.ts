44eab039bff93910ac9dcf894eb38a621ba57e8e
Szymon Soltysiak <szymon.soltysiak@gmail.com>
2018-12-12 22:40:01 +0100

    Add more actions.

--- src/state/actions/navigation.actions.ts
+++ src/state/actions/navigation.actions.ts
@@ -6,3 +6,7 @@ export type ActionsTypes = SetNavigationSectionAction;
 export const SetNavigationSectionType = '[NAVIGATION] SetNavigationSection';
 export type SetNavigationSectionAction = ReturnType<typeof SetNavigationSection>;
 export const SetNavigationSection = (section: Section) => createAction(SetNavigationSectionType, section);
+
+export const DetermineNavigationSectionType = '[NAVIGATION] DetermineNavigationSection';
+export type DetermineNavigationSectionAction = ReturnType<typeof DetermineNavigationSection>;
+export const DetermineNavigationSection = () => createAction(DetermineNavigationSectionType);
--- src/state/sagas/navigation.sagas.ts
+++ src/state/sagas/navigation.sagas.ts
@@ -1,8 +1,9 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, put } from 'redux-saga/effects';
 import { NavigationActions } from '../actions';
 
 export const logOnNavigateSaga = function* (action: NavigationActions.SetNavigationSectionAction) {
     console.log('***SAGA', action);
+    yield put(NavigationActions.DetermineNavigationSection());
 };
 
 export const NavigationSagas = [
