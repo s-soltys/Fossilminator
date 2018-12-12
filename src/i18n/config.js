d02788cc7a0d01e09599449aa941ca94a4e46393
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2018-12-12 20:07:51 +0100

    Rename to typescript.

--- src/i18n/config.js
+++
@@ -1,8 +0,0 @@
-import { I18n } from 'react-i18nify';
-
-const en = require('./languages/en.json');
-const pl = require('./languages/pl.json');
-
-I18n.setTranslations({ en, pl });
-
-I18n.setLocale('pl');
