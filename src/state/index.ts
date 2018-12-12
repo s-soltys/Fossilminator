17bc8dd3f63cbeccf855d5b834c8d61f0c7f7a38
Szymon Soltysiak <szymon.soltysiak@gmail.com>
2018-12-12 22:42:10 +0100

    Add redux dev tools.

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,22 +1,18 @@
 export * from './actions';
 export * from './constants';
 
-import { applyMiddleware, createStore, Store } from 'redux';
+import { applyMiddleware, compose, createStore, Store } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { AppState, InitialAppState } from './constants';
 import { RootReducer } from './reducers';
 import { RootSaga } from './sagas';
 
-// Configure Redux Dev Tools
-// let composeEnhancers = compose;
-// if ((process as any).browser) {
-//     composeEnhancers = (window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
-// }
+const composeEnhancers = (window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
 
 export const configureStore = (initialState: AppState = InitialAppState): Store<AppState> => {
     const sagaMiddleware = createSagaMiddleware();
 
-    const store: any = createStore(RootReducer, applyMiddleware(sagaMiddleware));
+    const store: any = createStore(RootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
     store.runSagaTask = () => (store.sagaTask = sagaMiddleware.run(RootSaga as any));
 
