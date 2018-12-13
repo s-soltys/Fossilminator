fc83ee1e9472be9002d3d03864da0537dd67b3f4
Ewa Lorecka <lorecka.ewa@gmail.com>
2018-12-13 18:53:03 +0000

    add logo

--- src/navigation/components/NavigationHeader.tsx
+++ src/navigation/components/NavigationHeader.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Translate } from 'react-i18nify';
+import { Logo } from '../../shared/components/Logo';
 
 export const NavigationHeader = () => (
     <header>
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
             <span className="navbar-brand">
+            <Logo />
                 <Translate value="application.title" />
             </span>
         </nav>
