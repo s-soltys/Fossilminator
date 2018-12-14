fd34e47ace45a5528acfb81d4a69dca0249382a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-14 22:15:26 +0000

    Deploy s-soltys/Fossilminator to github.com/s-soltys/Fossilminator.git:gh-pages

---
+++ precache-manifest.6d0b10618af615005c4c03186a9c2b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbd35c2f7d77afd67054",
+    "url": "/Fossilminator/static/css/main.929516e8.chunk.css"
+  },
+  {
+    "revision": "bbd35c2f7d77afd67054",
+    "url": "/Fossilminator/static/js/main.bbd35c2f.chunk.js"
+  },
+  {
+    "revision": "948e17a6e0701b6db593",
+    "url": "/Fossilminator/static/js/1.948e17a6.chunk.js"
+  },
+  {
+    "revision": "0ffb6c034d5066701561",
+    "url": "/Fossilminator/static/js/runtime~main.0ffb6c03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Fossilminator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1907dce7e459ab81aa05833b401685c",
+    "url": "/Fossilminator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Fossilminator/precache-manifest.773c1eba5cdf1a004bac4bf3599708e5.js"
+  "/Fossilminator/precache-manifest.6d0b10618af615005c4c03186a9c2b88.js"
 );
 
 workbox.clientsClaim();
