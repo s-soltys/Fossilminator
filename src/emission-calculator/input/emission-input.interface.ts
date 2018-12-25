1f50ed7ad681d77af72d6032680cf4765cff02db
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-25 11:51:19 +0100

    Rename stuff.

--- src/emission-calculator/input/emission-input.interface.ts
+++ src/emission-calculator/input/emission-input.interface.ts
@@ -1,3 +1,13 @@
+export interface EmissionInputHousing {}
+
+export interface EmissionInputPublicTransport {}
+
+export interface EmissionInputPrivateTransport {}
+
+export interface EmissionInputFood {}
+
+export interface EmissionInputConsumption {}
+
 export interface EmissionInputTransport {
     carKmPerWeek: number;
     annualHoursInAir: number;
