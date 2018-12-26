6b2d4480879016c4def456233e72cff430a21d6f
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-26 14:25:45 +0100

    More graphics.

--- src/emission-visualisation/components/Sprite.tsx
+++ src/emission-visualisation/components/Sprite.tsx
@@ -3,15 +3,16 @@ import React from "react";
 export const Sprite = ({ visible, file, alt, size, left, right, top, bottom }: any) => {
     if (!visible && typeof visible !== 'undefined') return null;
 
+    const style: any = {
+        position: 'absolute',
+        left: left && left + '%',
+        right: right && right + '%',
+        top: top && top + '%',
+        bottom: bottom && bottom + '%',
+        width: size + '%'
+    };
+
     return (
-        <img src={`images/ui/${file}`} alt={alt}
-            style={{
-                position: 'absolute',
-                left: left && left + '%',
-                right: right && right + '%',
-                top: top && top + '%',
-                bottom: bottom && bottom + '%',
-                width: size + '%'
-            }} />
+        <img src={`images/ui/${file}`} alt={alt} style={style} />
     );
 };
