88b65a76eebbff44f1de276ae4abe2248a7071c8
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-26 15:49:31 +0100

    Simple constants.

--- src/emission-calculator/calculator/partial-emission-results.ts
+++ src/emission-calculator/calculator/partial-emission-results.ts
@@ -8,25 +8,25 @@ type PartialEmissionResult = {
 
 export const PartialEmissionResult: PartialEmissionResult = {
     housingConstruction: (input) => {
-        return 0;
+        return 0.2;
     },
     housingHeating: (input) => {
-        return 0;
+        return 0.5;
     },
     warmWater: (input) => {
-        return 0;
+        return 1.25;
     },
     airConditioning: (input) => {
-        return 0;
+        return 0.12;
     },
     fuelForTransport: ({ transport }) => {
         return transport.carKmPerWeek * WeeksPerYear * TransportEmissionParams.perKmOfCarTravel;
     },
     carConstuction: (input) => {
-        return 0;
+        return 0.9;
     },
     publicTransport: (input) => {
-        return 0;
+        return 1.2;
     },
     airTravel: ({ transport }) => {
         return transport.annualHoursInAir * TransportEmissionParams.perAnnualHourInAir;
@@ -36,16 +36,16 @@ export const PartialEmissionResult: PartialEmissionResult = {
         return food.meatPerWeek * FoodEmissionParams.perMeatDailyMeatConsumption;
     },
     consumption: (input) => {
-        return 0;
+        return 2.6;
     },
     electricity: (input) => {
-        return 0;
+        return 1.1;
     },
     deforestation: (input) => {
-        return 0;
+        return 0.5;
     },
     commonServices: (input) => {
-        return 0;
+        return 3.5;
     },
 
     totalAnnualEmission: undefined
