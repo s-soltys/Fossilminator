cc9cda8a58081133f931a66fc7646f83780f6cec
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-26 11:15:42 +0100

    Create sprite component.

--- src/core/containers/SectionVisualisationContainer.tsx
+++ src/core/containers/SectionVisualisationContainer.tsx
@@ -4,6 +4,21 @@ import { bindActionCreators } from "redux";
 import { AppState, Section } from "../../state";
 import { VisualisationImage } from "../components";
 
+const Sprite = ({ file, alt, size, left, right, top, bottom }: any) => (
+    <img
+        style={{
+            position: 'absolute',
+            left: left && left + '%',
+            right: right && right + '%',
+            top: top && top + '%',
+            bottom: bottom && bottom + '%',
+            width: size + '%'
+        }}
+        src={`images/ui/${file}`}
+        alt={alt}
+    />
+);
+
 export class _SectionVisualisationContainer extends React.Component<any> {
     render() {
         const { section } = this.props;
@@ -12,19 +27,45 @@ export class _SectionVisualisationContainer extends React.Component<any> {
             case Section.Housing:
                 return (
                     <VisualisationImage name="314.jpg" alt="housing">
-                        <img style={{ position: 'absolute', left: '20%', bottom: '5%', width: '5%' }} src="images/ui/48.png" alt="person" />
+                        <Sprite file="48.png" alt="person" size="5" left="20" bottom="5" />
                     </VisualisationImage>
                 );
             case Section.WaterAndAirConditioning:
-                return <img className="w-100" src="images/ui/111.jpg" alt="water and air conditioning" />;
+                return (
+                    <img
+                        className="w-100"
+                        src="images/ui/111.jpg"
+                        alt="water and air conditioning"
+                    />
+                );
             case Section.PublicTransport:
-                return <img className="w-100" src="images/ui/367.jpg" alt="public transport" />;
+                return (
+                    <img
+                        className="w-100"
+                        src="images/ui/367.jpg"
+                        alt="public transport"
+                    />
+                );
             case Section.PrivateTransport:
-                return <img className="w-100" src="images/ui/96.jpg" alt="private transport" />;
+                return (
+                    <img
+                        className="w-100"
+                        src="images/ui/96.jpg"
+                        alt="private transport"
+                    />
+                );
             case Section.Food:
-                return <img className="w-100" src="images/ui/292.jpg" alt="food" />;
+                return (
+                    <img className="w-100" src="images/ui/292.jpg" alt="food" />
+                );
             case Section.Consumption:
-                return <img className="w-100" src="images/ui/392.jpg" alt="consumption" />;
+                return (
+                    <img
+                        className="w-100"
+                        src="images/ui/392.jpg"
+                        alt="consumption"
+                    />
+                );
             default:
                 return null;
         }
