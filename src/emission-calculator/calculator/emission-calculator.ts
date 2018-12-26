109452ade5ae305ddefc97b1e4a5e9f7c08f4ed6
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-26 15:47:40 +0100

    Compute everything.

--- src/emission-calculator/calculator/emission-calculator.ts
+++ src/emission-calculator/calculator/emission-calculator.ts
@@ -3,18 +3,18 @@ import { EmissionResult } from "../types/result";
 import { PartialEmissionResult } from "./partial-emission-results";
 
 export function calculateEmissionResults(input: EmissionInput): EmissionResult {
-    const data = Object.keys(PartialEmissionResult).reduce((result, key) => {
+    const partialResults = Object.keys(PartialEmissionResult).reduce((result, key) => {
         const fn = PartialEmissionResult[key];
         const value = fn ? fn(input) : 0;
         return { ...result, [key]: value };
-    }, {});
+    }, {}) as EmissionResult;
 
-    const totalAnnualEmission = Object.keys(data).reduce((sum, key) => sum + data[key], 0);
+    const totalAnnualEmission = Object.keys(partialResults).reduce((sum, key) => sum + partialResults[key], 0);
     
     const resultWithTotal = {
-        ...data,
+        ...partialResults,
         totalAnnualEmission
     };
 
-    return resultWithTotal as any;
+    return resultWithTotal;
 };
