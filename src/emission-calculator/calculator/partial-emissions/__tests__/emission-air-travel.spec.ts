9dcbf62702b13764050064affc4749d761c07b3c
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-29 13:39:04 +0100

    Add test cases.

--- src/emission-calculator/calculator/partial-emissions/__tests__/emission-air-travel.spec.ts
+++ src/emission-calculator/calculator/partial-emissions/__tests__/emission-air-travel.spec.ts
@@ -2,6 +2,6 @@ import { getAirTravelEmission } from "../emission-air-travel";
 
 describe('Partial emissions for AirTravel should', () => {
     it('handle empty inputs', () => {
-        expect(getAirTravelEmission({} as any)).toBe(0);
+        expect(getAirTravelEmission({ transport: {} } as any)).toBe(NaN);
     });
 });
