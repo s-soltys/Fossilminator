10243c42441fa10fc8e813fa981fa31cbfa0630a
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-29 14:55:00 +0100

    Use empty emission.

--- src/emission-calculator/constants/empty-emission-input.constant.ts
+++ src/emission-calculator/constants/empty-emission-input.constant.ts
@@ -1,15 +1,32 @@
-import { EmissionInput } from '../types/input';
+import { EmissionInput, PlaneClass } from '../types/input';
 
 export const EmptyEmissionInput: EmissionInput = {
     country: {
         countryCode: null,
-        energeticMix: {} as any
+        energeticMix: {
+            coal: 0,
+            nuclear: 0,
+            solar: 0,
+            water: 0,
+            wind: 0
+        }
     },
     transport: {} as any,
     housing: {} as any,
     water: {} as any,
     privateTransport: {} as any,
-    publicTransport: {} as any,
+    publicTransport: {
+        cityBusTravelWeekly: null,
+        longDistanceBusTravelWeekly: null,
+        microBusTravelWeekly: null,
+        metroTramTravelWeekly: null,
+        taxiTravelWeekly: null,
+        suburbanTrainTravelWeekly: null,
+        longDistanceTrainTravelWeekly: null,
+        airClass: PlaneClass.Economy,
+        shortDistanceAirTravelAnnualHours: null,
+        longDistanceAirTravelAnnualHours: null
+    },
     food: {} as any,
     consumption: {} as any,
 };
--- src/state/constants/initial-state.constant.ts
+++ src/state/constants/initial-state.constant.ts
@@ -1,33 +1,11 @@
 import { AppState, Section } from "./state.interface";
+import { EmptyEmissionInput } from "../../emission-calculator/constants";
 
 export const InitialAppState: AppState = {
     navigation: {
         section: Section.Housing
     },
-    emissionInput: {
-        country: {
-            countryCode: 'pl',
-            energeticMix: {
-                coal: 0,
-                nuclear: 0,
-                solar: 0,
-                water: 0,
-                wind: 0
-            }
-        },
-        transport: {
-            carKmPerWeek: null,
-            annualHoursInAir: null
-        },
-        publicTransport: {} as any,
-        privateTransport: {} as any,
-        housing: {} as any,
-        water: {} as any,
-        consumption: {} as any,
-        food: {
-            meatPerWeek: 1
-        } as any
-    },
+    emissionInput: EmptyEmissionInput,
     emissionResult: {
         housingConstruction: 0,
         housingHeating: 0,
