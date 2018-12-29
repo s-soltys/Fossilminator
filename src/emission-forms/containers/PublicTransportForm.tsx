b189de71ca52f3f9df26a71ae055da98f380c8f7
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-29 12:55:01 +0100

    Add more public transport computation.

--- src/emission-forms/containers/PublicTransportForm.tsx
+++ src/emission-forms/containers/PublicTransportForm.tsx
@@ -21,18 +21,70 @@ class _PublicTransportForm extends React.Component<Props> {
                         <div>
                             <LabelledInput
                                 label="transport.cityBusTravelWeeklyKmTitle"
-                                addon="units.hours"
+                                addon="units.km"
                                 placeholder="transport.cityBusTravelWeeklyKmPlaceholder"
                                 value={data.cityBusTravelWeeklyKm}
                                 valueChange={value => update({ cityBusTravelWeeklyKm: value })}>
                             </LabelledInput>
                         </div>
+                        <div className="pt-3">
+                            <LabelledInput
+                                label="transport.longDistanceBusTravelWeeklyKmTitle"
+                                addon="units.km"
+                                placeholder="transport.longDistanceBusTravelWeeklyKmPlaceholder"
+                                value={data.longDistanceBusTravelWeeklyKm}
+                                valueChange={value => update({ longDistanceBusTravelWeeklyKm: value })}>
+                            </LabelledInput>
+                        </div>
                     </div>
                     <div className="col-12 col-md-6 col-lg-4">
-                        
+                        <div>
+                            <LabelledInput
+                                label="transport.microBusTravelWeeklyKmTitle"
+                                addon="units.km"
+                                placeholder="transport.microBusTravelWeeklyKmPlaceholder"
+                                value={data.microBusTravelWeeklyKm}
+                                valueChange={value => update({ microBusTravelWeeklyKm: value })}>
+                            </LabelledInput>
+                        </div>
+                        <div className="pt-3">
+                            <LabelledInput
+                                label="transport.metroTramTravelWeeklyKmTitle"
+                                addon="units.km"
+                                placeholder="transport.metroTramTravelWeeklyKmPlaceholder"
+                                value={data.metroTramTravelWeeklyKm}
+                                valueChange={value => update({ metroTramTravelWeeklyKm: value })}>
+                            </LabelledInput>
+                        </div>
                     </div>
                     <div className="col-12 col-md-6 col-lg-4">
-                        
+                        <div>
+                            <LabelledInput
+                                label="transport.taxiTravelWeeklyKmTitle"
+                                addon="units.km"
+                                placeholder="transport.taxiTravelWeeklyKmPlaceholder"
+                                value={data.taxiTravelWeeklyKm}
+                                valueChange={value => update({ taxiTravelWeeklyKm: value })}>
+                            </LabelledInput>
+                        </div>
+                        <div className="pt-3">
+                            <LabelledInput
+                                label="transport.suburbanTrainTravelWeeklyKmTitle"
+                                addon="units.km"
+                                placeholder="transport.suburbanTrainTravelWeeklyKmPlaceholder"
+                                value={data.suburbanTrainTravelWeeklyKm}
+                                valueChange={value => update({ suburbanTrainTravelWeeklyKm: value })}>
+                            </LabelledInput>
+                        </div>
+                        <div className="pt-3">
+                            <LabelledInput
+                                label="transport.longDistanceTrainTravelWeeklyKmTitle"
+                                addon="units.km"
+                                placeholder="transport.longDistanceTrainTravelWeeklyKmPlaceholder"
+                                value={data.longDistanceTrainTravelWeeklyKm}
+                                valueChange={value => update({ longDistanceTrainTravelWeeklyKm: value })}>
+                            </LabelledInput>
+                        </div>
                     </div>
                 </div>
                 <div className="row">
