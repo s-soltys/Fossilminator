e47eb448144975a233b2d10cd88421ae5c849a00
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2018-12-29 15:20:13 +0100

    Add more changes.

--- src/emission-calculator/types/input/index.ts
+++ src/emission-calculator/types/input/index.ts
@@ -1,6 +1,3 @@
-export * from './emission-input-types.enum';
-export * from './emission-input.interface';
-
 export * from "./emission-input-country.interface";
 export * from "./emission-input-water.interface";
 export * from "./emission-input-housing.interface";
@@ -8,3 +5,5 @@ export * from "./emission-input-public-transport.interface";
 export * from "./emission-input-consumption.interface";
 export * from "./emission-input-food.interface";
 export * from "./emission-input-private-transport.interface";
+
+export * from './emission-input.interface';
