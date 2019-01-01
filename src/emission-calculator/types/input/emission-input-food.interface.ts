5df08398de61cfec3370c42f3849aa5704150c53
Szymon Soltysiak <szymon.soltysiak@zenhomes.de>
2019-01-01 20:10:04 +0100

    Add options.

--- src/emission-calculator/types/input/emission-input-food.interface.ts
+++ src/emission-calculator/types/input/emission-input-food.interface.ts
@@ -1,10 +1,10 @@
 export interface EmissionInputFood {
-    averageAmountOfFood: number; // Mało, Średnio itd.
-    foodSourceLocality: number; // Jem lokalną bez marketów, mały sklep z zyw. lokalna, Towary lokalne i sezonowe, Nie zwracam uwagi na sezonowosc, Egzotyczne
-    meatPerWeek: number; // Brak produktów zw. (weganin), Bez mięsa (wegetarianin), 1-2 razy, 3-6 razy, codziennie
-    frozenFoodWeekly: number; // wcale, rzadko, 1x tydzień, 2-3 tygodniowo, codziennie
-    refridgeratorEfficiency: number; // Brak, Niskoelektywa G-D, Średioefektywna C-B, Energooszczędna A, Bardzo A+
-    ovenEfficiency: number; // Elektryczna zwykła, indukcyjna, gazowa, węglowa, Drewno
+    averageAmountOfFood: number;
+    foodSourceLocality: number;
+    meatPerWeek: number;
+    frozenFoodWeekly: number;
+    refridgeratorEfficiency: number;
+    ovenEfficiency: number;
     additionalFreezer: boolean;
     dishwasher: boolean;
 }
--- src/emission-forms/containers/FoodForm.tsx
+++ src/emission-forms/containers/FoodForm.tsx
@@ -5,9 +5,53 @@ import { AppState, EmissionInputActions } from '../../state';
 import { EmissionFormWrapper, LabelledDropdown } from '../components';
 import { EmissionInputFood } from '../../emission-calculator';
 
-const OPTIONS = [
-    { value: 1, label: 'food.someoption' },
-    { value: 2, label: 'food.someoptions' }
+const averageAmountOfFoodOptions = [
+    { value: 1, label: 'food.averageAmountOfFood.veryLow' },
+    { value: 2, label: 'food.averageAmountOfFood.low' },
+    { value: 3, label: 'food.averageAmountOfFood.medium' },
+    { value: 4, label: 'food.averageAmountOfFood.high' },
+    { value: 5, label: 'food.averageAmountOfFood.veryHigh' }
+];
+
+const foodSourceLocalityOptions = [
+    { value: 1, label: 'food.foodSourceLocality.localDireclyFromSource' },
+    { value: 2, label: 'food.foodSourceLocality.localSmallStores' },
+    { value: 3, label: 'food.foodSourceLocality.localAndSeasonal' },
+    { value: 4, label: 'food.foodSourceLocality.dontCare' },
+    { value: 5, label: 'food.foodSourceLocality.exotic' }
+];
+
+const meatPerWeekOptions = [
+    { value: 1, label: 'food.meatPerWeek.vegan' },
+    { value: 2, label: 'food.meatPerWeek.vegetarian' },
+    { value: 3, label: 'food.meatPerWeek.rarely' },
+    { value: 4, label: 'food.meatPerWeek.oncePerWeek' },
+    { value: 5, label: 'food.meatPerWeek.fewTimesPerWeek' },
+    { value: 5, label: 'food.meatPerWeek.daily' } 
+];
+
+const frozenFoodWeeklyOptions = [
+    { value: 1, label: 'food.frozenFoodWeekly.never' },
+    { value: 2, label: 'food.frozenFoodWeekly.rarely' },
+    { value: 3, label: 'food.frozenFoodWeekly.weekly' },
+    { value: 4, label: 'food.frozenFoodWeekly.fewTimesPerWeek' },
+    { value: 5, label: 'food.frozenFoodWeekly.daily' }
+];
+
+const refridgeratorEfficiencyOptions = [
+    { value: 1, label: 'food.refridgeratorEfficiency.none' },
+    { value: 2, label: 'food.refridgeratorEfficiency.low' },
+    { value: 3, label: 'food.refridgeratorEfficiency.medium' },
+    { value: 4, label: 'food.refridgeratorEfficiency.high' },
+    { value: 5, label: 'food.refridgeratorEfficiency.veryHigh' }
+];
+
+const ovenEfficiencyOptions = [
+    { value: 1, label: 'food.ovenEfficiency.normalElectric' },
+    { value: 2, label: 'food.ovenEfficiency.induction' },
+    { value: 3, label: 'food.ovenEfficiency.gas' },
+    { value: 4, label: 'food.ovenEfficiency.coal' },
+    { value: 5, label: 'food.ovenEfficiency.wood' }
 ];
 
 interface Props {
@@ -28,7 +72,7 @@ class _FoodForm extends React.Component<Props> {
                                 label="food.averageAmountOfFood.title"
                                 value={data.averageAmountOfFood}
                                 valueChange={value => update({ averageAmountOfFood: value })}
-                                options={OPTIONS}>
+                                options={averageAmountOfFoodOptions}>
                             </LabelledDropdown>
                         </div>
                         <div className="pt-3">
@@ -36,7 +80,7 @@ class _FoodForm extends React.Component<Props> {
                                 label="food.foodSourceLocality.title"
                                 value={data.foodSourceLocality}
                                 valueChange={value => update({ foodSourceLocality: value })}
-                                options={OPTIONS}>
+                                options={foodSourceLocalityOptions}>
                             </LabelledDropdown>
                         </div>
                         <div className="pt-3">
@@ -44,7 +88,7 @@ class _FoodForm extends React.Component<Props> {
                                 label="food.meatPerWeek.title"
                                 value={data.meatPerWeek}
                                 valueChange={value => update({ meatPerWeek: value })}
-                                options={OPTIONS}>
+                                options={meatPerWeekOptions}>
                             </LabelledDropdown>
                         </div>
                     </div>
@@ -54,7 +98,7 @@ class _FoodForm extends React.Component<Props> {
                                 label="food.frozenFoodWeekly.title"
                                 value={data.frozenFoodWeekly}
                                 valueChange={value => update({ frozenFoodWeekly: value })}
-                                options={OPTIONS}>
+                                options={frozenFoodWeeklyOptions}>
                             </LabelledDropdown>
                         </div>
                         <div className="pt-3">
@@ -62,7 +106,7 @@ class _FoodForm extends React.Component<Props> {
                                 label="food.refridgeratorEfficiency.title"
                                 value={data.refridgeratorEfficiency}
                                 valueChange={value => update({ refridgeratorEfficiency: value })}
-                                options={OPTIONS}>
+                                options={refridgeratorEfficiencyOptions}>
                             </LabelledDropdown>
                         </div>
                         <div className="pt-3">
@@ -70,7 +114,7 @@ class _FoodForm extends React.Component<Props> {
                                 label="food.ovenEfficiency.title"
                                 value={data.ovenEfficiency}
                                 valueChange={value => update({ ovenEfficiency: value })}
-                                options={OPTIONS}>
+                                options={ovenEfficiencyOptions}>
                             </LabelledDropdown>
                         </div>
                     </div>
