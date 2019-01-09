a6760740f99bc7232a44f41d1eba83aa543ba122
Szymon Soltysiak <szymon.soltysiak@gmail.com>
2019-01-10 00:00:02 +0100

    Readd badge.

--- src/shared/components/EmissionAlertBadge.tsx
+++ src/shared/components/EmissionAlertBadge.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Translate } from 'react-i18nify';
 import { Alert } from 'reactstrap';
 
-export const EmissionAlertBadge = () => (
+export const EmissionAlertBadge = ({ emission }: any) => (
     <Alert color="warning">
         <Translate value="emissions.yourEmissionsAreTooHigh" />
     </Alert>
