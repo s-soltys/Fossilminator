295c4ba92604ea873b4260fa6d74be9d9fffcf3f
ewal2 <38157680+ewal2@users.noreply.github.com>
2019-01-22 13:53:45 +0100

    add two emission´s messages (#37)
    * add two emission´s messages
    
    * add fun to EmissionAlertBadge
    
    * Change EmissionAlert func

--- src/shared/components/EmissionAlertBadge.tsx
+++ src/shared/components/EmissionAlertBadge.tsx
@@ -3,9 +3,25 @@ import { Translate } from 'react-i18nify';
 import { Alert } from 'reactstrap';
 
 export const EmissionAlertBadge = ({ emission }: any) => {
-    return (
-        <Alert color="warning">
-            <Translate value="emissions.yourEmissionsAreTooHigh" />
-        </Alert>
-    )
+    if(emission > 20){
+        return(
+             <Alert color="danger">
+                <Translate value="emissions.yourEmissionsAreTooHigh" />
+             </Alert>
+        )
+    }
+    else if(emission >= 10){
+        return(
+            <Alert color="warning">
+                <Translate value="emissions.yourEmissionsAreStandard" />
+            </Alert>
+        )
+    }
+    else{
+        return(
+            <Alert color="success">
+                <Translate value="emissions.yourEmissionsAreLow" />
+            </Alert> 
+        )
+    }
 };
